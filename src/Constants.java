da52fed1aafbb73dc456174d54679c504dde4031
Emil Erlandsson <emilerlandsson@live.com>
2015-05-27 13:14:10 +0200

    shiet JUST GUT RIIIL

--- src/Constants.java
+++ src/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static final String BLOG_URL="http://localhost:8080/blog/main.jsp";
 
     public static final String CHROME_DRIVER ="webdriver.chrome.driver";
-    public static final String PATH_TO_CHROME_DRIVER="C:/Users/Richard/Downloads/Selenium/chromedriver_win32/chromedriver.exe";
+    public static final String PATH_TO_CHROME_DRIVER="C:\\Users\\Emil\\SkyDrive\\GitHub\\SeleniumTesterG4blog\\.idea\\extra libs\\selenium-2.45.0\\chromedriver.exe";
 
     public static final String YOUTUBE_URL="https://www.youtube.com";
 
